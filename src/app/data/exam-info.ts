83e2ea2cde66764452714ab7b997cfdc81cc64c0
Weeabird2 <patrik_brat@aol.com>
2026-07-01 14:34:55 +0200

    init

--- src/app/data/exam-info.ts
+++ src/app/data/exam-info.ts
@@ -1,4 +1,7 @@
+import { TopicInfo } from './topic-info';
+
 export interface ExamInfo {
   id: string;
   title: string;
+  topics: TopicInfo[];
 }
---
+++ src/app/data/topic-info.ts
@@ -0,0 +1,4 @@
+export interface TopicInfo {
+  id: string;
+  title: string;
+}
