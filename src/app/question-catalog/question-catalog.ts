04b4ff1a817ffd8c06b608373b8d9dd8828d9cbb
Weeabird2 <patrik_brat@aol.com>
2026-07-02 10:11:16 +0200

    question-catalog

--- src/app/question-catalog/question-catalog.ts
+++ src/app/question-catalog/question-catalog.ts
@@ -3,10 +3,15 @@ import { RouterLink } from '@angular/router';
 import { Catalog } from '../service/catalog';
 @Component({
   selector: 'app-question-catalog',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './question-catalog.html',
   styleUrl: './question-catalog.css',
 })
 export class QuestionCatalog {
+  openExamId?: string;
   constructor(public catalogService: Catalog) {}
+
+  toggleExam(examId: string): void {
+    this.openExamId = this.openExamId === examId ? undefined : examId;
+  }
 }
